610b14fa702000ae2696729d041885e30f749000
joselupe2002 <joselupe_2002@hotmail.com>
2020-04-07 14:00:07 -0500

    Mantenimientoi

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
 		<link rel="icon" type="image/gif" href="imagenes/login/sigea.ico">
-		<title>Sistema de Gesti&oacute;n Escolar-Administrativa</title>
+		<title>Sistema de Gesti&oacute;n Escolar</title>
 		<meta name="description" content="User login page" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 		
