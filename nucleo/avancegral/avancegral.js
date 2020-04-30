195f7d98e2c20e5555f25cd28f2e272d0f4a191e
joselupe2002 <joselupe_2002@hotmail.com>
2020-04-30 08:42:17 -0500

    prueba

--- nucleo/avancegral/avancegral.js
+++ nucleo/avancegral/avancegral.js
@@ -21,6 +21,9 @@ contMat=1;
 			type: "GET",
 			url:  "../base/getSesion.php?bd=Mysql&campo=carrera",
 			success: function(data){  
+				alert("SELECT CARR_CLAVE, CARR_DESCRIP FROM ccarreras where CARR_ACTIVO='S'"+
+				" and CARR_CLAVE IN ("+data+")");
+
 				addSELECT("selCarreras","lascarreras","PROPIO", "SELECT CARR_CLAVE, CARR_DESCRIP FROM ccarreras where CARR_ACTIVO='S'"+
 				" and CARR_CLAVE IN ("+data+")", "",""); 
 				},
