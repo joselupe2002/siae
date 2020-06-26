0cfd973b4951e99cbe10a8e019d6ba6a5b7e077d
joselupe2002 <joselupe_2002@hotmil.com>
2020-06-26 17:56:42 -0500

    reportehorario

--- nucleo/vcargasprof/horario.php
+++ nucleo/vcargasprof/horario.php
@@ -9,10 +9,92 @@
 	$logouser="../../imagenes/login/sigea.png";
 	$nivel="../../";
 
+	class VariableStream
+	{
+		private $varname;
+		private $position;
+	
+		function stream_open($path, $mode, $options, &$opened_path)
+		{
+			$url = parse_url($path);
+			$this->varname = $url['host'];
+			if(!isset($GLOBALS[$this->varname]))
+			{
+				trigger_error('Global variable '.$this->varname.' does not exist', E_USER_WARNING);
+				return false;
+			}
+			$this->position = 0;
+			return true;
+		}
+	
+		function stream_read($count)
+		{
+			$ret = substr($GLOBALS[$this->varname], $this->position, $count);
+			$this->position += strlen($ret);
+			return $ret;
+		}
+	
+		function stream_eof()
+		{
+			return $this->position >= strlen($GLOBALS[$this->varname]);
+		}
+	
+		function stream_tell()
+		{
+			return $this->position;
+		}
+	
+		function stream_seek($offset, $whence)
+		{
+			if($whence==SEEK_SET)
+			{
+				$this->position = $offset;
+				return true;
+			}
+			return false;
+		}
+		
+		function stream_stat()
+		{
+			return array();
+		}
+	}
 
 	
    class PDF extends FPDF {
-   	        
+			   
+			function __construct($orientation='P', $unit='mm', $size='A4')
+			{
+				parent::__construct($orientation, $unit, $size);
+				// Register var stream protocol
+				stream_wrapper_register('var', 'VariableStream');
+			}
+
+			function MemImage($data, $x=null, $y=null, $w=0, $h=0, $link='')
+			{
+				// Display the image contained in $data
+				$v = 'img'.md5($data);
+				$GLOBALS[$v] = $data;
+				$a = getimagesize('var://'.$v);
+				if(!$a)
+					$this->Error('Invalid image data');
+				$type = substr(strstr($a['mime'],'/'),1);
+				$this->Image('var://'.$v, $x, $y, $w, $h, $type, $link);
+				unset($GLOBALS[$v]);
+			}
+
+			function GDImage($im, $x=null, $y=null, $w=0, $h=0, $link='')
+			{
+				// Display the GD image associated with $im
+				ob_start();
+				imagepng($im);
+				$data = ob_get_clean();
+				$this->MemImage($data, $x, $y, $w, $h, $link);
+			}
+	/*============================================================================================*/
+	
+
+
    	     var $eljefe="";
    	       
    	       function getDatosPersona($num){   		       
@@ -246,7 +328,10 @@
 		
 		$dataEmpl = $pdf->getDatosPersona($_GET["ID"]);
 
-		$pdf->Image($dataEmpl[0]["EMPL_FOTO"],10,40,20);
+	
+		$logo = file_get_contents($dataEmpl[0]["EMPL_FOTO"]);
+		$pdf->MemImage($logo,10,40,20);
+
 		
 		$pdf->Ln(10);
 		$pdf->setX(40); $pdf->SetFont('Montserrat-ExtraBold','B',8); $pdf->Cell(0,0,"NOMBRE: ",0,0,'L');	
