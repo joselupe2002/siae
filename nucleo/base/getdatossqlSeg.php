6344ee77a1f6a4b537524db7dd12d33f6c821bc7
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-05 12:29:11 -0500

    p9

--- nucleo/base/getdatossqlSeg.php
+++ nucleo/base/getdatossqlSeg.php
@@ -3,6 +3,17 @@
 
    session_start(); 
    if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"])  ){ 
+      include("../.././includes/Conexion.php");
+
+       $miConex = new Conexion();
+       $datos = array();
+       $res=$miConex->getConsulta($_POST['bd'],$_POST['sql']);
+       
+       foreach ($res as $lin) {
+       	 echo ("dato: ".$lin);  
+          $datos[]=$lin;
+       }
+       
       echo "si entre ".$_SESSION['inicio']."|".$_SESSION['idsesion']."==".$_POST["dato"];
 
    }
