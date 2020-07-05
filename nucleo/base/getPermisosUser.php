30625c197e3d12a357985db147adb0c3bf1e64b2
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-05 14:48:47 -0500

    p3

--- nucleo/base/getPermisosUser.php
+++ nucleo/base/getPermisosUser.php
@@ -1,8 +1,7 @@
 <?php 
      session_start(); if (($_SESSION['inicio']==1)){ 
-    header('Content-Type: text/html; charset=ISO-8859-1'); 
-	
-	mb_internal_encoding ('ISO-8859-1');
+   // header('Content-Type: text/html; charset=ISO-8859-1'); 
+	// mb_internal_encoding ('ISO-8859-1');
 	
    include("../.././includes/Conexion.php");
    include("./../../includes/UtilUser.php");
