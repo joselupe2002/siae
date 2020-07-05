e24763c0334bd510c4c0f9e169134df556051529
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-05 12:43:01 -0500

    T5

--- nucleo/base/getdatossqlfiltro.php
+++ nucleo/base/getdatossqlfiltro.php
@@ -1,8 +1,10 @@
 <?php 
     session_start(); if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"]) ){ 
+   
+    /*    
     header('Content-Type: text/html; charset=ISO-8859-1'); 
-	
-	mb_internal_encoding ('ISO-8859-1');
+    mb_internal_encoding ('ISO-8859-1');
+    */
 	
    include("../.././includes/Conexion.php");
    include("../.././includes/UtilUser.php");
