857f92f54075708c0d359aff7c3d817e348cd182
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-05 12:26:30 -0500

    p8

--- nucleo/base/getdatossqlSeg.php
+++ nucleo/base/getdatossqlSeg.php
@@ -1,7 +1,11 @@
 <?php 
  
 
-   session_start(); if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"])  ){ 
+   session_start(); 
+   echo "si entre ".$_SESSION['inicio']."|".$_SESSION['idsesion']."==".$_POST["dato"];
+
+   /*
+   if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"])  ){ 
    header('Content-Type: text/html; charset=UTF-8'); 
    echo "si entre ".$_SESSION['idsesion']."==".$_POST["dato"];
    
