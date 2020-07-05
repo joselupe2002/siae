9845275d0fa3a67b61558707ed92bdb9ab0dff2d
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-05 12:35:53 -0500

    CAMBIOS

--- nucleo/base/getdatossqlSeg.php
+++ nucleo/base/getdatossqlSeg.php
@@ -1,29 +1,8 @@
 <?php 
- 
-
-   session_start(); 
-   if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"])  ){ 
-      include("../.././includes/Conexion.php");
-
-       $miConex = new Conexion();
-       $datos = array();
-       $res=$miConex->getConsulta($_POST['bd'],$_POST['sql']);
-       
-       foreach ($res as $lin) {
-       	 echo ("dato: ".$lin);  
-          $datos[]=$lin;
-       }
-      
-       echo json_encode($datos);
-
-   }
-   else {echo "no entre";}
-   /*
-      header('Content-Type: text/html; charset=UTF-8'); 
-   echo "si entre ".$_SESSION['idsesion']."==".$_POST["dato"];
-   
-
-	mb_internal_encoding ('UTF-8');
+     session_start(); if (($_SESSION['inicio']==1)  && ($_SESSION['idsesion']==$_POST["dato"])  ){ 
+    header('Content-Type: text/html; charset=ISO-8859-1'); 
+	
+	mb_internal_encoding ('ISO-8859-1');
 	
    include("../.././includes/Conexion.php");
 
@@ -32,12 +11,10 @@
        $res=$miConex->getConsulta($_POST['bd'],$_POST['sql']);
        
        foreach ($res as $lin) {
-       	 echo ("dato: ".$lin);  
+       	  
           $datos[]=$lin;
        }
        echo json_encode($datos);
     
-    
 } else {header("Location: index.php");}
-*/
 ?>
