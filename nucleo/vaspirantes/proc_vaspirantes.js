5788a10f66b9e77a64e8f0c341396d9170065330
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-09 14:13:50 -0500

    cambio correo

--- nucleo/vaspirantes/proc_vaspirantes.js
+++ nucleo/vaspirantes/proc_vaspirantes.js
@@ -665,6 +665,7 @@ function envioCorreo(modulo,usuario,essuper) {
 			  "              <option value=\"0\">Elija una opción</option> "+
 			  "              <option value=\"1\">Clave y Usuario</option>"+
 			  "              <option value=\"2\">Correo General</option>"+
+			  "              <option value=\"3\">Aviso Exámen</option>"+
 			  "         </select>"+
 			  "     </div>"+
 			  "    <div class=\"col-sm-6\" style=\"text-align:justify;\">"+
@@ -708,6 +709,33 @@ function cargaMsj(){
 		" PARA COLOCAR NEGRITAS ENCIERRE ENTRE <b>ESTO VA NEGRITA</b>\n"+
 		" PARA COLOCAR INFORMACIÓN DEL ASPIRANTE UN CAMPO {TITULO_COLUMNA} EJEMPLO {NOMBRE}\n"+
 		" PARA COLOCAR UN ENLACE <a href=\"https://escolar.webcoretic.com/admision/\">Texto del Enlace</a><BR/>\n"
+		);
+	}
+
+	if  ($("#opcorreo").val()=='3') {
+		$("#elasunto").val("ITSM: INFORMACIÓN SOBRE EXÁMEN DE ADMISIÓN");
+		$("#msjCorreo").html("<b>AVISO IMPORTANTE ASIPIRANTES AL ITSM</b> <br/><br/>"+
+							 "El Instituto Tecnológico Superior de Macuspana atendiendo las "+
+							 " disposiciones del Consejo de Salubridad General de nuestro país y con la finalidad "+
+							 " de prevenir la propagación del SARS-CoV-2 (COVID-19), comunica a TODOS SUS ASPIRANTES "+
+							 " que la aplicación del <b>EXAMEN DE ADMISIÓN</b> se realizará en la <b>MODALIDAD EN LÍNEA,</b> "+
+							 " el <b>LUNES 03 DE AGOSTO</b>, para lo que deberá atender las siguientes disposiciones: <br/><br/>"+ 
+		
+		"1.-  Tener digital o impreso su ficha de ingreso.<br/>"+
+		"<br/>2.- Tener la clave de  acceso al examen  que se  les enviará al correo electrónico registrado en su solicitud  de ingreso, con la información necesaria para responder el examen,<br/>"+
+		"<br/>3.- Ingresar al examen en línea desde un  dispositivo  electrónico  con  acceso a internet ( PC de escritorio, laptop, tablet o teléfono celular). a través  de la URL <br/>"+
+		" <a href=\"https://escolar.macuspana.tecnm.mx/admision/\"> https://escolar.macuspana.tecnm.mx/admision/</a> <br/>"+
+		"<br/>4.- El horario del examen es de 9:00 a 13:00 hrs., solicitamos tu puntual  ingreso.<br/>"+
+		"<br/>5.- Para dudas y aclaraciones del examen,  vía whatsApp al numero <b> 9361298912 </b> y al correo electrónico <b>escolares@macuspana.tecnm.mx</b><br/>"+
+		"<br/>6.- En caso de  interrupción  por   cuestiones  de  internet o energía  eléctrica, el examen guarda las respuestas hasta donde te hayas q uedado y podrás continuar al resolver tu imprevisto técnico.<br/>"+
+		"<br/>7.- El examen consta de distintas áreas de conocimiento, por lo que al terminar de responder las preguntas de cada sección, deberás dar click en el botón siguiente para continuar resolviendo el examen. Cuando hayas contestado todas las preguntas, deberás dar click en el botón finalizar  y tu examen se guardara automáticamente.<br/>"+
+		"<br/>8.- Publicación de resultados el Martes 04 de agosto de 2020, en nuestra página web <a href=\"https://macuspana.tecnm.mx\"> https://macuspana.tecnm.mx </a> <br/>"+
+		
+		"<br/><b>Les sugerimos estar atentos a las redes sociales oficiales del instituto.</b><br/>"+
+		
+		"<b>Facebook: <a href=\"https://www.facebook.com/tecnologico.macuspana.73\"> Itsm Tecnologico Macuspana</b><br/>"+
+		"<b>Instragram: itsdemacuspana</b><br/>"
+		
 		);
 	}
 
