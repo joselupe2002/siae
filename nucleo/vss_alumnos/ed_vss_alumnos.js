1517c49367819d8b4b17290ca2d0082c3cab186c
joselupe2002 <joselupe_2002@hotmil.com>
2020-07-22 08:56:48 -0500

    SERV SOC

--- nucleo/vss_alumnos/ed_vss_alumnos.js
+++ nucleo/vss_alumnos/ed_vss_alumnos.js
@@ -7,3 +7,38 @@ function changeCALIFICACIONL(elemento, usuario, institucion, campus){
 
 }
 
+function calculaCalif(){
+    var prom=60;
+    var cal1=parseInt($("#REP1EVAL").val())+parseInt($("#REP1AUTO").val());
+    var cal2=parseInt($("#REP2EVAL").val())+parseInt($("#REP2AUTO").val());
+    var cal3=parseInt($("#REP3EVAL").val())+parseInt($("#REP3AUTO").val());
+
+   
+        prom=((cal1+cal2+cal3)/3).toFixed(0);
+ 
+    $("#CALIFICACION").val(prom);
+    $('#CALIFICACION').prop('disabled', 'disabled');
+
+}
+
+function changeREP1EVAL(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
+
+function changeREP1AUTO(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
+   function changeREP2EVAL(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
+
+function changeREP2AUTO(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
+   function changeREP3EVAL(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
+
+function changeREP3AUTO(elemento, usuario, institucion, campus){
+    calculaCalif();
+   }
