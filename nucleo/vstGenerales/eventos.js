083958a566d4bdb76bfc58bb502b6babcc08c7b3
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-03 12:47:48 -0500

    alumnos asess

--- nucleo/vstGenerales/eventos.js
+++ nucleo/vstGenerales/eventos.js
@@ -97,3 +97,43 @@ function cargaAlumnosRep (contenedor, ciclo,carrera,genero){
     });	  
 
 }
+
+
+
+function cargaAlumnosRepAs(contenedor, ciclo,carrera,genero){
+
+	elsqlMa=elsql="select  ALUCTR AS MATRICULA, CONCAT(ALUM_NOMBRE,' ',ALUM_APEPAT,' ',ALUM_APEMAT) AS NOMBRE, "+
+	" ALUM_CARRERAREG, CARR_DESCRIP,  if (ALUM_SEXO=1,'H','M') AS SEXO , COUNT(*) AS REP,"+
+	" (select count(*) from vasesorias where ASES_MATRICULA=ALUCTR AND ASES_CICLO='"+ciclo+"') as ASESORIAS "+
+	" from dlista, falumnos b, cmaterias c, ccarreras d where ALUCTR=ALUM_MATRICULA  "+
+	" AND ALUM_CARRERAREG='"+carrera+"' and MATCVE=MATE_CLAVE "+
+	" and ALUM_CARRERAREG=CARR_CLAVE "+
+	" and PDOCVE IN ('"+ciclo+"') AND IFNULL(MATE_TIPO,'0') NOT IN ('OC','T','RP','I','SS') "+
+	" AND LISCAL<70 and ALUM_SEXO='"+genero+"'"+
+	" and ALUCTR IN (select a.ASES_MATRICULA from vasesorias a where a.ASES_CICLO='"+ciclo+"')'";
+	" GROUP BY ALUCTR,ALUM_CARRERAREG, CARR_DESCRIP, ALUM_SEXO ";
+
+	parametros={sql:elsqlMa,dato:sessionStorage.co,bd:"Mysql"}
+
+	$.ajax({
+		type: "POST",
+		data:parametros,
+		url:  "../base/getdatossqlSeg.php",
+		success: function(data){  						      			      
+			cad="<ol>";
+			jQuery.each(JSON.parse(data), function(clave, valor) {
+				elcolor='success';
+			   cad+="<li style=\"text-align:justify;\">"+valor.MATRICULA+" "+valor.NOMBRE+
+			            "<span title=\"Asesorias que llevó\" class=\"badge badge-"+elcolor+"\">"+valor.ASESORIAS+"</span>"+					
+			        "</li>"; 
+			});	
+			cad+="</ol>";
+			
+			mostrarIfo("infoMaterias",contenedor,"Alumnos con asignaturas reprobadas y Asesorías",cad,"modal-lg"); 
+	 },
+	 error: function(dataMat) {	                  
+			 alert('ERROR: '+dataMat);
+						 }
+    });	  
+
+}
