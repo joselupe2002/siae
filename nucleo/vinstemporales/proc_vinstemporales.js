22044e5da1f585f274058c564a7b0f6ed6317f60
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-05 09:27:33 -0500

    sss

--- nucleo/vinstemporales/proc_vinstemporales.js
+++ nucleo/vinstemporales/proc_vinstemporales.js
@@ -5,7 +5,7 @@ var elReg=0;
 function verPagoTem(modulo,usuario,essuper){
 	table = $("#G_"+modulo).DataTable();
 	if (table.rows('.selected').data().length>0) {
-		abrirPesta(table.rows('.selected').data()[0]["RUTA"]);
+		abrirPesta(table.rows('.selected').data()[0]["RUTA"],"Recibo");
 		// window.open(table.rows('.selected').data()[0]["RUTA"], '_blank'); 	    
 	}
 	else {
