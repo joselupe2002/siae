926acb1248bca3b6fe1fb0729695c7a0e6bda26f
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-14 12:48:00 -0500

    ffff

--- aspirantes/ficha.php
+++ aspirantes/ficha.php
@@ -282,13 +282,13 @@
                 $dataFoto = $this->LoadFoto();
                 $miutil = new UtilUser();    
                      
-                /*
+                
                 if (!empty($dataFoto)) { 
                     $lafoto=$dataFoto[0][0]; 
                     $logo = file_get_contents($lafoto);
                     $this->MemImage($logo,20,37,22,28);
                 }
-                */
+            
 
                  
                 $fecha=date("d/m/Y"); 
