97ad979bf8ebb21a8fa6415d49b1c7f617fac148
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-14 12:44:44 -0500

    WEEE

--- aspirantes/ficha.php
+++ aspirantes/ficha.php
@@ -281,12 +281,14 @@
                 $dataCiclo = $this->LoadDatosCiclo();
                 $dataFoto = $this->LoadFoto();
                 $miutil = new UtilUser();    
-                              
+                     
+                /*
                 if (!empty($dataFoto)) { 
                     $lafoto=$dataFoto[0][0]; 
                     $logo = file_get_contents($lafoto);
                     $this->MemImage($logo,20,37,22,28);
                 }
+                */
 
                  
                 $fecha=date("d/m/Y"); 
