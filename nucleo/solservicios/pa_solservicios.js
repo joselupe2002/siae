bcc45ba0726605a571f2c56a0b360d90c633a3d0
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-17 19:53:37 -0500

    ddd

--- nucleo/solservicios/pa_solservicios.js
+++ nucleo/solservicios/pa_solservicios.js
@@ -183,8 +183,10 @@ function generaTablaInformacion(grid_data){
 			'ID',valor.IDDET,'RECIBO DE PAGO '+txtop,'eadjreinsres','alta',valor.IDDET,cadRuta,activaEliminar);	
 		}
 		else {
-			$("#file"+valor.IDDET).html("<a href=\""+valor.RUTARES+"\" target=\"_blank\" >"+
-						"<img src=\"../../imagenes/menu/pdf.png\" style=\"width:25px; height:25px;\"></a>");				
+			if (valor.RUTARES=="") {
+		      	$("#file"+valor.IDDET).html("<a href=\""+valor.RUTARES+"\" target=\"_blank\" >"+
+						"<img src=\"../../imagenes/menu/pdf.png\" style=\"width:25px; height:25px;\"></a>");
+			}				
 		}
 
 		n++;
