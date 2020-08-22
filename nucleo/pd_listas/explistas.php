a5e0e53f245fe5bf65286c0693f2373c93bd36b5
joselupe2002 <joselupe_2002@hotmil.com>
2020-08-21 19:34:32 -0500

    ffff

--- nucleo/pd_listas/explistas.php
+++ nucleo/pd_listas/explistas.php
@@ -5,14 +5,14 @@ session_start(); if (($_SESSION['inicio']==1)) {
 
 echo "entre";
 
-/*
+
 require_once '../../PHPExcel/Classes/PHPExcel.php';
 require_once '../../PHPExcel/Classes/PHPExcel/IOFactory.php';
 require_once '../../PHPExcel/Classes/PHPExcel/Reader/Excel2007.php';
 include("../.././includes/Conexion.php");
 $miConex = new Conexion();
 
-
+/*
 $objPHPExcel = new PHPExcel();
 $objReader = PHPExcel_IOFactory::createReader('Excel2007');
 $objPHPExcel = $objReader->load("../../file/Lista.xlsx");
