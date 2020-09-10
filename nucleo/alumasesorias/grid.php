c8b84654165972d8028f32509542cdc26c27229e
joselupe2002 <joselupe_2002@hotmil.com>
2020-09-09 20:04:27 -0500

    ddddd

--- nucleo/alumasesorias/grid.php
+++ nucleo/alumasesorias/grid.php
@@ -212,7 +212,7 @@
 <script type="text/javascript">
 		var todasColumnas;
 		var usuario="<?php echo $_SESSION["usuario"];	?>";
-		var nombreuser="<?php echo $_SESSION["usuario"];	?>";
+		var nombreuser="<?php echo $_SESSION["nombre"];	?>";
 		$(document).ready(function($) { var Body = $('body'); Body.addClass('preloader-site');});
 		$(window).load(function() {$('.preloader-wrapper').fadeOut();$('body').removeClass('preloader-site');});
 
