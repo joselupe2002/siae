e2ae74135c7119a044f899d75716d2dc6a2bb09f
joselupe2002 <joselupe_2002@hotmil.com>
2020-11-11 09:02:38 -0600

    CONSULTA DE LIBROS

--- nucleo/bib_consultas/bib_consultas.js
+++ nucleo/bib_consultas/bib_consultas.js
@@ -12,25 +12,9 @@ contMat=1;
     jQuery(function($) { 
 	
 
-		$("#lascarreras").append("<span class=\"label label-warning\">Carrera</span>");
-		 
-		$.ajax({
-			type: "GET",
-			url:  "../base/getSesion.php?bd=Mysql&campo=carrera",
-			success: function(data){  
-				addSELECT("selCarreras","lascarreras","PROPIO", "SELECT CARR_CLAVE, CARR_DESCRIP FROM ccarreras where CARR_ACTIVO='S'"+
-				" and CARR_CLAVE IN ("+data+")", "",""); 
-				},
-			error: function(data) {	                  
-					   alert('ERROR: '+data);
-					   $('#dlgproceso').modal("hide");  
-				   }
-		   });
+		$("#lascarreras").append("<span class=\"label label-warning\">Carrera</span>");		
+		addSELECT("selSecciones","lascarreras","PROPIO", "SELECT ID, SECCION FROM bib_secciones ", "",""); 
 		
-		$("#losciclossel").append("<span class=\"label label-danger\">Ciclo Escolar</span>");
-		addSELECT("selCiclos","losciclossel","PROPIO", "SELECT CICL_CLAVE, concat(CICL_CLAVE,' ',CICL_DESCRIP) FROM ciclosesc order by cicl_clave desc", "","");  			      
-	
-
 		$("#losciclos").append("<i class=\" fa white fa-level-down bigger-180\"></i> ");
 		$("#losciclos").append("<strong><span id=\"elciclo\" class=\"text-white bigger-40\"></span></strong>");
 		colocarCiclo("elciclo","CLAVE");
@@ -49,18 +33,20 @@ contMat=1;
 /*===========================================================POR MATERIAS ==============================================*/
 function cargarInformacion(){
 	script="";
+	lapal="";
 
-	   $("#informacion").empty();
-	   $("#informacion").append(script);
-			
-	elsql="select DISTINCT(ALUM_MATRICULA), ALUM_NOMBRE, ALUM_APEPAT, ALUM_APEMAT, ALUM_MAPA, ALUM_ACTIVO, "+
-	"ALUM_FOTO, CARR_DESCRIP"+
-	" from falumnos a, ccarreras c where ALUM_CARRERAREG=CARR_CLAVE "+
-	" AND ALUM_MATRICULA IN (SELECT ALUCTR FROm dlista where PDOCVE='"+$("#selCiclos").val()+"')"+
-	" and ALUM_CARRERAREG='"+$("#selCarreras").val()+"' ORDER BY ALUM_APEPAT, ALUM_APEMAT, ALUM_NOMBRE";
+	$("#informacion").empty();
+	$("#informacion").append(script);
+	
+	lascarr="";
+    if ($("#selSecciones").val()!="0") { lascarr=" AND SECCION='"+$("#selSecciones").val()+"'";}
+	lapal=$("#palabra").val().toUpperCase();
+	cad="";
+	if (lapal!='') { cad=" and (AUTOR LIKE '%"+lapal+"%' or TITULO LIKE '%"+lapal+"%')";}
+	elsql="select * from vbib_ejemplares where  TIPO='LIBROS'  AND ACCESIBLE=3 "+lascarr+cad+" order by IDFICHA";
 
 
-	mostrarEspera("esperahor","grid_misalumnos","Cargando Datos...");
+	mostrarEspera("esperahor","grid_bib_consultas","Cargando Datos...");
 	parametros={sql:elsql,dato:sessionStorage.co,bd:"Mysql"}
 	$.ajax({
 		   type: "POST",
@@ -78,48 +64,35 @@ function cargarInformacion(){
 
 function generaTabla(grid_data){	
 contAlum=1;
-$("#cuerpoMaterias").empty();
 $("#contenido").empty();
 $("#tabMaterias").append("<tbody id=\"cuerpoMaterias\">");
 
 jQuery.each(grid_data, function(clave, valor) { 
-	lafoto=valor.ALUM_FOTO;
-	if ((valor.ALUM_FOTO ==null) ||(valor.ALUM_FOTO =="")){lafoto="../../imagenes/menu/default.png";} 
+	lafoto=valor.FOTO_LIBRO;
+	if ((valor.FOTO_LIBRO ==null) ||(valor.FOTO_LIBRO =="")){lafoto="../../imagenes/menu/default.png";} 
 	
-	activo="red";
-	if (valor.ALUM_ACTIVO==1) {activo="green"}
     $("#contenido").append(		
-		"	<div class=\"itemdiv memberdiv\" id=\""+valor.ALUM_MATRICULA+"\">"+
+		"	<div class=\"itemdiv memberdiv\" id=\""+valor.ID+"\">"+
 		"		<div class=\"ma_principal\">"+		
 		"				<a href=\"#\"><img src=\""+lafoto+"\" class=\"ma_foto\"  /></a><br/>"+		
 		"			<div class=\"body\">"+
 		"				<div class=\"name fontRoboto\">"+
-		"					<a href=\"#\"><i class=\"fa "+activo+" fa-circle \"></i> "+
-		"                   <span class=\"elname\" mipadre=\""+valor.ALUM_MATRICULA+"\">"+valor.ALUM_MATRICULA+" "+valor.ALUM_APEPAT+" "+valor.ALUM_APEMAT+" "+valor.ALUM_NOMBRE+"</span></a>"+
+		"					<a href=\"#\">"+
+		"                   <span class=\"elname\" mipadre=\""+valor.ID+"\">"+valor.TITULO+
+		"							<span class=\"hidden\">"+valor.AUTOR+"</span></span></a>"+
 		"			</div>"+
 		"		</div>"+
 		"		<div class=\"popover ma_popover\">"+
 		"			<div class=\"arrow\"></div>"+
 		"			<div class=\"popover-content\">"+
-		"				<div class=\"bolder\">"+valor.ALUM_APEPAT+" "+valor.ALUM_APEMAT+" "+valor.ALUM_NOMBRE+"</div>"+
-		"					<div class=\"time\"><i class=\"ace-icon fa fa-road middle bigger-120 orange\"></i><span class=\"green\">"+valor.CARR_DESCRIP+"</span></div>"+
+		"				<div class=\"bolder\">"+valor.TITULO+"</div>"+
+		"					<div class=\"time\"><i class=\"ace-icon fa fa-user middle bigger-120 orange\"></i><span class=\"green\">"+valor.AUTOR+"</span></div>"+
+		"					<div class=\"time\"><i class=\"ace-icon fa fa-columns  middle bigger-120 purple\"></i><span class=\"blue\"> ANAQUEL: "+valor.ANAQUEL+"</span></div>"+
 		"					<div class=\"hr dotted hr-8\"></div>"+
 		"					<div class=\"tools action-buttons\">"+
-		"						<a title=\"Ver Avance Curricular\" onclick=\"verAvanceAlum('"+valor.ALUM_MATRICULA+"');\" style=\"cursor:pointer;\">"+
+		"						<a title=\"Ver Avance Curricular\" onclick=\"reservar('"+valor.ID+"');\" style=\"cursor:pointer;\">"+
 		"                            <i class=\"ace-icon fa fa-bar-chart-o blue bigger-150\"></i>"+
-		"                       </a>"+
-		"						<a title=\"Ver Kardex\" onclick=\"verKardex('"+valor.ALUM_MATRICULA+"');\" style=\"cursor:pointer;\">"+
-		"                            <i class=\"ace-icon fa fa-file-text-o green bigger-150\"></i>"+
-		"                       </a>"+
-		"						<a title=\"Ver Calificaciones del ciclo actual\" onclick=\"verCalifCiclo('"+valor.ALUM_MATRICULA+"','"+valor.ALUM_APEPAT+" "+valor.ALUM_APEMAT+" "+valor.ALUM_NOMBRE+"');\" style=\"cursor:pointer;\">"+
-		"                            <i class=\"ace-icon fa fa-list-alt pink bigger-150\"></i>"+
-		"                       </a>"+
-		"						<a title=\"Horario de clases\" onclick=\"verHorario('"+valor.ALUM_MATRICULA+"','"+valor.ALUM_APEPAT+" "+valor.ALUM_APEMAT+" "+valor.ALUM_NOMBRE+"');\" style=\"cursor:pointer;\">"+
-		"                            <i class=\"ace-icon fa fa-calendar red bigger-150\"></i>"+
-		"                       </a>"+
-		"						<a title=\"Actividades Complementarias\" onclick=\"verActCom('"+valor.ALUM_MATRICULA+"','"+valor.ALUM_APEPAT+" "+valor.ALUM_APEMAT+" "+valor.ALUM_NOMBRE+"');\" style=\"cursor:pointer;\">"+
-		"                            <i class=\"ace-icon fa fa-thumb-tack yellow bigger-150\"></i>"+
-		"                       </a>"+
+		"                       </a>"+				
 		"					</div>"+
 		"				</div>"+
 		"			</div>"+
