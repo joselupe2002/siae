11d0565e1a5c718d525e2c64f6d5f1f2625c5aa2
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-31 17:45:17 -0600

    sss

--- aspirantes/ficha.php
+++ aspirantes/ficha.php
@@ -333,15 +333,27 @@ class VariableStream
                 $this->SetFont('Montserrat-Black','',10);   
                 $this->SetFillColor(172,31,8);
                 $this->SetTextColor(255);                     
-                $this->Cell(85,5,utf8_decode("1RA OPCIÓN"),1,0,'C',true);
-                $this->Cell(85,5,utf8_decode("2DA OPCIÓN"),1,0,'C',true);
+                $this->Cell(40,5,utf8_decode("CLAVE"),1,0,'C',true);
+                $this->Cell(130,5,utf8_decode("CARRERA"),1,0,'C',true);
+                $this->Ln(5);
+
+                $this->SetFont('Montserrat-Black','',8);  
+                $this->SetFillColor(172,31,6);                
+                $this->SetTextColor(16, 119, 233); 
+                $this->Cell(40,5,utf8_decode($dataAlum[0]["CLAVE"]),1,0,'C');
+                $this->Cell(130,5,utf8_decode($dataAlum[0]["CARRERAD"]),1,0,'C');
+                $this->Ln(5);
+
+                $this->SetFont('Montserrat-Black','',10);   
+                $this->SetFillColor(172,31,8);
+                $this->SetTextColor(255);                      
+                $this->Cell(170,5,utf8_decode("2DA OPCIÓN"),1,0,'C',true);
                 $this->Ln(5);
 
                 $this->SetFont('Montserrat-Black','',8);  
                 $this->SetFillColor(172,31,6);
-                $this->SetTextColor(0);
-                $this->Cell(85,5,utf8_decode($dataAlum[0]["CARRERAD"]),1,0,'C');
-                $this->Cell(85,5,utf8_decode($dataAlum[0]["CARRERAD2"]),1,0,'C');
+                $this->SetTextColor(0);                
+                $this->Cell(170,5,utf8_decode($dataAlum[0]["CARRERAD2"]),1,0,'C');
                 $this->Ln(5);
 
 
